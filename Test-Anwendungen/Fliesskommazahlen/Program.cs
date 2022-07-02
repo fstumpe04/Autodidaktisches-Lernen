94b05c311023a4944df1bbc3908a1704ba2ce4a1
Florian Stumpe <fstumpe04@gmail.com>
2022-07-02 15:19:09 +0200

    4. Video Fließkommazahlen wurde hinzugefügt

---
+++ Test-Anwendungen/Fliesskommazahlen/Program.cs
@@ -0,0 +1,17 @@
+﻿// float-Werte
+Console.WriteLine($"Die niedrigste Floatgröße lautet: {float.MinValue}");
+Console.WriteLine($"Die höchste Floatgröße lautet: {float.MaxValue}");
+Console.WriteLine($"Der Epsilon-Wert von float lautet: {float.Epsilon}");
+Console.WriteLine($"Der NaN-Wert von float lautet: {float.NaN}");
+Console.WriteLine($"Der NegativeInfinity-Wert von float lautet: {float.NegativeInfinity}");
+Console.WriteLine($"Der PositiveInfinity-Wert von float lautet: {float.PositiveInfinity}");
+
+Console.WriteLine();
+
+// double-Werte
+Console.WriteLine($"Die niedrigste Floatgröße lautet: {double.MinValue}");
+Console.WriteLine($"Die höchste Floatgröße lautet: {double.MaxValue}");
+Console.WriteLine($"Der Epsilon-Wert von float lautet: {double.Epsilon}");
+Console.WriteLine($"Der NaN-Wert von float lautet: {double.NaN}");
+Console.WriteLine($"Der NegativeInfinity-Wert von float lautet: {double.NegativeInfinity}");
+Console.WriteLine($"Der PositiveInfinity-Wert von float lautet: {double.PositiveInfinity}");
---
+++ Test-Anwendungen/IntegerGanzzahlen/Program.cs
@@ -0,0 +1,57 @@
+﻿// sbyte-Werte
+Console.WriteLine($"Die niedrigste sbyte größe lautet: {sbyte.MinValue}");
+Console.WriteLine($"Die höchste sbyte größe lautet: {sbyte.MaxValue}");
+
+Console.WriteLine();
+
+// byte-Werte
+Console.WriteLine($"Die niedrigste byte größe lautet: {byte.MinValue}");
+Console.WriteLine($"Die höchste byte größe lautet: {byte.MaxValue}");
+
+Console.WriteLine();
+
+// short-Werte
+Console.WriteLine($"Die niedrigste short größe lautet: {short.MinValue}");
+Console.WriteLine($"Die höchste byte größe lautet: {short.MaxValue}");
+
+Console.WriteLine();
+
+// ushort-Werte
+Console.WriteLine($"Die niedrigste ushort größe lautet: {ushort.MinValue}");
+Console.WriteLine($"Die höchste ubyte größe lautet: {ushort.MaxValue}");
+
+Console.WriteLine();
+
+// int-Werte
+Console.WriteLine($"Die niedrigste int größe lautet: {int.MinValue}");
+Console.WriteLine($"Die höchste int größe lautet: {int.MaxValue}");
+
+Console.WriteLine();
+
+// uint-Werte
+Console.WriteLine($"Die niedrigste uint größe lautet: {uint.MinValue}");
+Console.WriteLine($"Die höchste uint größe lautet: {uint.MaxValue}");
+
+Console.WriteLine();
+
+// long-Werte
+Console.WriteLine($"Die niedrigste long größe lautet: {long.MinValue}");
+Console.WriteLine($"Die höchste long größe lautet: {long.MaxValue}");
+
+Console.WriteLine();
+
+// ulong-Werte
+Console.WriteLine($"Die niedrigste ulong größe lautet: {ulong.MinValue}");
+Console.WriteLine($"Die höchste ulong größe lautet: {ulong.MaxValue}");
+
+Console.WriteLine();
+
+// nint-Werte
+Console.WriteLine($"Die niedrigste nint größe lautet: {nint.MinValue}");
+Console.WriteLine($"Die höchste nint größe lautet: {nint.MaxValue}");
+
+Console.WriteLine();
+
+// nuint-Werte
+Console.WriteLine($"Die niedrigste nuint größe lautet: {nuint.MinValue}");
+Console.WriteLine($"Die höchste nuint größe lautet: {nuint.MaxValue}");
