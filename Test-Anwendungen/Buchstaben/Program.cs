f70aaaf92b65fd19d2b49487f39ae8a463bfcc97
Florian Stumpe <fstumpe04@gmail.com>
2022-07-03 12:39:23 +0200

    5. Video Buchstaben Character hinzugefügt

---
+++ Test-Anwendungen/Buchstaben/Program.cs
@@ -0,0 +1,7 @@
+﻿// char
+Console.WriteLine($"Die niedrigste char größe lautet: {char.MinValue}");
+Console.WriteLine($"Die höchste char größe lautet: {char.MaxValue}");
+for (long i = 0; i <= char.MaxValue; i++)
+{
+    Console.WriteLine($"DerBuchstabe an Stelle {i} lautet {(char)i}");
+}
