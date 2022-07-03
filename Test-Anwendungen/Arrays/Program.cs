578de212a9f47261222e613b05af3c6484bfc360
Florian Stumpe <fstumpe04@gmail.com>
2022-07-03 14:39:50 +0200

    7. Video Arrays hinzugefügt

--- Test-Anwendungen/Arrays/Program.cs
+++ Test-Anwendungen/Arrays/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] a = new int[3];
+// a[0] = 1;
+// a[1] = 2;
+// a[2] = 42;
+int[] a = {1, 2, 42};
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"{a[i]}");
+}
