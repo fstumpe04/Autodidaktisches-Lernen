6779c8fc2ff7571aac0fc73e4500365f7fa7731a
Florian Stumpe <fstumpe04@gmail.com>
2022-07-03 14:35:11 +0200

    6. Video Kommentare hinzugefügt

---
+++ Test-Anwendungen/Kommentare/Program.cs
@@ -0,0 +1,12 @@
+﻿// Einzeiliger Kommentar
+/* 
+Mehrzeiliger Kommentar
+*/
+// TODO: Schlüsselwort um in Visual Studio eine Aufgabe zu definieren.
+// HACK: Schlüsselwort um in Visual Studio eine Aufgabe zu definieren.
+// NOTE: Schlüsselwort um in Visual Studio eine Notiz zu definieren.
+// UNDONE:
+/// <summary>
+/// Zusammenfassung einer Klasse oder einer Funktion oder eines Namespace.
+/// </summary>
+/// <param name="args">Parametererklärung</param>
