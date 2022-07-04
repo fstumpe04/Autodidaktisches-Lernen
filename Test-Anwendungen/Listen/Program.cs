8467902340e1d331aaa766b25d94608dff94a963
Florian Stumpe <fstumpe04@gmail.com>
2022-07-04 11:41:29 +0200

    9. Video Listen hinzugefügt.

---
+++ Test-Anwendungen/Listen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+// Liste
+List<int> a = new List<int>();
+a.Add(42);
+foreach (var item in a)
+{
+    System.Console.WriteLine(item);   
+}
+System.Console.WriteLine($"Die Länge der Liste lautet {a.Count()}");
+
+LinkedList<int> b = new LinkedList<int>();
+b.AddFirst(42);
+System.Console.WriteLine($"Die Länge der Linked Liste lautet {a.Count()}");
