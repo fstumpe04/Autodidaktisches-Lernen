695db9e780ed9166a51104527a32e295107a899d
Florian Stumpe <fstumpe04@gmail.com>
2022-07-06 14:17:40 +0200

    19. Video break und continue hinzugefügt.

---
+++ Test-Anwendungen/break und continue/Program.cs
@@ -0,0 +1,46 @@
+﻿// continue
+string[] meinStringArray = {"42", "dieses", "tut", "ist", "geil"};
+foreach (string element in meinStringArray)
+{
+    System.Console.WriteLine("abc");
+    continue;
+    System.Console.WriteLine(element);
+}
+/*
+abc
+abc
+abc
+abc
+abc
+*/
+foreach (string element in meinStringArray)
+{
+    if (element == "42")
+    {
+        continue;   
+    }
+    System.Console.WriteLine(element);
+}
+/*
+dieses
+tut
+ist
+geil
+*/
+
+// break
+meinStringArray = new string[] {"dieses", "tut", "ist", "geil","42", "hi", "wtf", "ist"};
+foreach (string element in meinStringArray)
+{
+    System.Console.WriteLine(element);
+    if (element == "geil")
+    {
+        break;   
+    }
+}
+/*
+dieses
+tut
+ist
+geil
+*/
