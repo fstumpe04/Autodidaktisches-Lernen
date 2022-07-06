e32b54c86b9b007214a7ffa945c7513606d2daff
Florian Stumpe <fstumpe04@gmail.com>
2022-07-06 12:38:34 +0200

    18. Video foreach Schleifen hinzugefügt.

---
+++ Test-Anwendungen/foreach Schleifen/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] meinIntArray = {42, 1337, 42, 42, 42};
+foreach (int element in meinIntArray)
+{
+    System.Console.WriteLine(element);
+}
+
+string[] meinStringArray = {"42", "dieses", "tut", "ist", "geil"};
+foreach (string element in meinStringArray)
+{
+    System.Console.WriteLine(element);
+}
