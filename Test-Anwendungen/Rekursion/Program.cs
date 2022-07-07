db1ad87eff875bcf1b75c7461f0e7a51a4af61a5
Florian Stumpe <fstumpe04@gmail.com>
2022-07-07 19:23:13 +0200

    24. Video Default Argumente hinzugefügt.

---
+++ Test-Anwendungen/Rekursion/Program.cs
@@ -0,0 +1,43 @@
+﻿// Fakultät
+int Fakul (int n)
+{
+    int ret = 0;
+    if (n <= 2)
+    {
+        ret = n;
+    }
+    else
+    {
+        ret = n * Fakul(n - 1);
+    }
+    return ret;
+}
+
+int meineVar = Fakul(10);
+System.Console.WriteLine(meineVar);
+
+// Fibonacci-Folge
+int Fibo(int n)
+{
+    int ret = 0;
+    if (n >= 13)
+    {
+        ret = n;
+    }
+    else
+    {
+        ret = n * Fibo(n);
+    }
+    System.Console.WriteLine(ret); 
+    return ret;
+}
+// Fibo(13)
+// --> 1
+// ----> 1
+// ------> 2
+// --------> 3
+// ----------> 5
+// ------------> 8
+// --------------> 13
+
+meineVar = Fibo(1);
