263c8a5f6f449d896550fb463033865c4b80ce8a
Florian Stumpe <fstumpe04@gmail.com>
2022-07-07 14:53:38 +0200

    22. Video Parameter hinzugefügt.

---
+++ Test-Anwendungen/Parameter/Program.cs
@@ -0,0 +1,38 @@
+﻿// int foo(bool abc)
+// {
+//     int ret = 0;
+//     int ergebnis = 42*1337;
+//     if (abc)
+//     {
+//         ret = ergebnis;
+//     }
+//     else
+//     {
+//         ret = 0;
+//     }
+//     return ret;
+// }
+
+// int meinInteger = foo(true);
+// int meinInteger2 = foo(false);
+
+// System.Console.WriteLine(meinInteger + "\n" + meinInteger2);
+
+int foo(bool abc, int def, int hij)
+{
+    int ret = 0;
+    int ergebnis = def*hij;
+    if (abc)
+    {
+        ret = ergebnis;
+    }
+    else
+    {
+        ret = 0;
+    }
+    return ret;
+}
+bool meinBool = true;
+int meinInteger = foo(meinBool, 42, 1337);
+
+System.Console.WriteLine(meinInteger);
