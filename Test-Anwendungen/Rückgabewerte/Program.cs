6259805bb84e5070fa6797fd09acdc26910f60e7
Florian Stumpe <fstumpe04@gmail.com>
2022-07-07 14:00:25 +0200

    21. Video Rückgabewerte wurde hinzugefügt.

---
+++ Test-Anwendungen/Rückgabewerte/Program.cs
@@ -0,0 +1,25 @@
+﻿// void foo()
+// {
+
+// }
+
+// foo();
+int foo()
+{
+    bool abc = true;
+    int ret = 0;
+    int ergebnis = 42*1337;
+    if (abc)
+    {
+        ret = ergebnis;
+    }
+    else
+    {
+        ret = 0;
+    }
+    return ret;
+}
+
+int meinInteger = foo();
+
+System.Console.WriteLine(meinInteger);
