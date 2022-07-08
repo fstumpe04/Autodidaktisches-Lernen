d92dc9b312d04a54465a864b3fdb22777e239d82
Florian Stumpe <fstumpe04@gmail.com>
2022-07-08 16:25:11 +0200

    26. Video Lazy Evaluation hinzugefügt.

---
+++ Test-Anwendungen/Call-by-Value vs Call-by-Reference/Program.cs
@@ -0,0 +1,16 @@
+﻿// void foo(int a){
+//     a = 1337;
+// }
+// int a = 42;
+// foo(a);
+// System.Console.WriteLine(a); // 42
+
+void foo(ref List<int> list){
+    list = new List<int>() {4, 5, 6};
+}
+List<int> list = new List<int>() {1, 2, 3};
+foo(ref list);
+foreach (var item in list)
+{
+    System.Console.WriteLine(item); // 4 , 5, 6
+}
