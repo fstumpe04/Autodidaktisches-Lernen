166fa3d1fe9feca010fad7aaef10cad041df0276
Florian Stumpe <fstumpe04@gmail.com>
2022-07-08 17:15:43 +0200

    27. Video Funktionszeiger hinzugefügt.

---
+++ Test-Anwendungen/Funktionszeiger/Program.cs
@@ -0,0 +1,25 @@
+﻿// int foo()
+// {
+//     return 1;
+// }
+//var meineFkt = foo;
+//System.Console.WriteLine(meineFkt()); //1
+
+// int bar(Func<int> baz)
+// {
+//     return baz();
+// }
+
+var meineFkt = foo;
+// System.Console.WriteLine(bar(meineFkt));
+
+int foo (int n, int n2)
+{
+    return n * n2;
+}
+int bar (Func<int> baz, int n)
+{
+    n2 = 1337;
+    return baz(n, n2);
+}
+System.Console.WriteLine(bar(foo, 42));
