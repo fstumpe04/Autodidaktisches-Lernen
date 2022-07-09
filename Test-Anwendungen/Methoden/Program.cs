e8b58d5499bdf0a08071801b6718207f9da33cf6
Florian Stumpe <fstumpe04@gmail.com>
2022-07-09 17:24:00 +0200

    32. Video Methoden hinzugefügt.

---
+++ Test-Anwendungen/Methoden/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schule youtube = new Schule();
+            youtube.name = "The Morpheus Tutorials";
+            youtube.anzahlSchueler = 13000;
+            youtube.neuerSchueler();
+            System.Console.WriteLine(youtube.anzahlSchueler);
+        }
+    }
+}
---
+++ Test-Anwendungen/Methoden/Schule.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Methoden
+{
+    public class Schule
+    {
+        public string name;
+        public int anzahlSchueler;
+        public void neuerSchueler()
+        {
+            this.anzahlSchueler++;
+        }
+    }
+}
