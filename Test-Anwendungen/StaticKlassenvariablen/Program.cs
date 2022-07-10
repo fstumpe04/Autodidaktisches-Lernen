4a7cf86f4177c77b57fbb684b19e7519a5f5f725
Florian Stumpe <fstumpe04@gmail.com>
2022-07-10 18:10:28 +0200

    35. Video Static - Klassenvariablen hinzugefügt.

---
+++ Test-Anwendungen/StaticKlassenvariablen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticKlassenvariablen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schule kit = new Schule();
+            string currentGesetzesgrundlage = kit.getGesetzesgrundlage();
+            Console.WriteLine(currentGesetzesgrundlage);
+        }
+    }
+}
---
+++ Test-Anwendungen/StaticKlassenvariablen/Schule.cs
@@ -0,0 +1,11 @@
+namespace StaticKlassenvariablen
+{
+    class Schule
+    {
+        private static string gesetzesgrundlage = "SchG";
+        public static string getGesetzesgrundlage()
+        {
+            return Schule.gesetzesgrundlage;
+        }
+    }
+}
