8d8c8981f57ccd9f5c608b270b2ba36cdb598ee0
Florian Stumpe <fstumpe04@gmail.com>
2022-07-14 17:34:27 +0200

    40. Video Enums hinzugefügt.

---
+++ Test-Anwendungen/Enums/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(((byte)Richtung.RECHTS));
+        }
+        enum Richtung
+        {
+            HOCH, RUNTER, LINKS, RECHTS            
+        }
+    }
+}
