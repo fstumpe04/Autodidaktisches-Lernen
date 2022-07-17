1eb9919614d8fdabc556b91c40049ef98e7cdd22
Florian Stumpe <fstumpe04@gmail.com>
2022-07-17 10:32:56 +0200

    47. Video Statische innere Klassen hinzugefügt.

---
+++ Test-Anwendungen/StatischeInnereKlassen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatischeInnereKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schule schule = new Schule();
+        }
+    }
+}
---
+++ Test-Anwendungen/StatischeInnereKlassen/Schule.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+namespace StatischeInnereKlassen
+{
+    public class Schule
+    {
+        public static class Schueler
+        {
+            public static int alter;
+            public static string name;
+        }
+        public int AnzahlSchueler { get; set; }
+        public void NeuerSchueler()
+        {
+            AnzahlSchueler += 1;
+        }
+    }
+}
