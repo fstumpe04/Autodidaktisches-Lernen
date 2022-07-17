25a09025d3b8d58f5e2547adc78cb94aec1cd5e7
Florian Stumpe <fstumpe04@gmail.com>
2022-07-17 12:30:54 +0200

    49. Video anonyme inner Klassen hinzugefügt.

---
+++ Test-Anwendungen/AnonymeInnereKlassen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymeInnereKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Test-Anwendungen/AnonymeInnereKlassen/Schule.cs
@@ -0,0 +1,7 @@
+namespace AnonymeInnereKlassen
+{
+    abstract class Schule
+    {
+        public void SchuleAus();
+    }
+}
