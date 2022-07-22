1fc7e1b63bce13785c108750bcf4d56336ba557c
Florian Stumpe <fstumpe04@gmail.com>
2022-07-22 17:27:09 +0200

    54. Video Generische Funktionen hinzugefügt.

---
+++ Test-Anwendungen/Generische Funktionen/Container.cs
@@ -0,0 +1,10 @@
+namespace Generische_Funktionen
+{
+    public class Container<T>
+    {
+        T[] Sort(T[] elements)
+        {
+            return elements;
+        }
+    }
+}
---
+++ Test-Anwendungen/Generische Funktionen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Generische_Funktionen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container container = new Container();
+            String[] meinArray = {"verstecke champion", ";", "seit 1958"};
+            meinArray = container.
+            System.Console.WriteLine(meinArray);
+        }
+    }
+}
