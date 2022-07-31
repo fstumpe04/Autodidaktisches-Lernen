48d194760f272c8aff1609afd014e3f9e70c64a3
Florian Stumpe <fstumpe04@gmail.com>
2022-07-31 10:53:21 +0200

    Video 74 Dateien hinzugefügt.

---
+++ Test-Anwendungen/Dateien/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Dateien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "meineDatei.txt";
+            File.Create(filename);
+            File.OpenWrite(filename);
+            string[] zeilen = File.ReadAllLines(filename);
+            for (int i = 0; i < zeilen.Length; i++)
+            {
+                System.Console.WriteLine(zeilen[i]);
+            }
+        }
+    }
+}
