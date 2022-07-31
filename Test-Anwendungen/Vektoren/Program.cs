c1953f4ea710ef83beb60e3e13702a36c1c1ac1e
Florian Stumpe <fstumpe04@gmail.com>
2022-07-31 08:44:39 +0200

    Video 72 Vektoren hinzugefügt.

---
+++ Test-Anwendungen/Vektoren/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace Vektoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(1,0,1);
+            Vector b = new Vector(0,0,0);
+            System.Console.WriteLine(a+b);
+        }
+    }
+}
