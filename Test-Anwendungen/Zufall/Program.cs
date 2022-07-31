b29e6b3ca17c656195f519867399bfd31a23a874
Florian Stumpe <fstumpe04@gmail.com>
2022-07-31 10:13:51 +0200

    Video 73 Zufall hinzugefügt.

---
+++ Test-Anwendungen/Zufall/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zufall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int i = random.Next(1, 6);
+            System.Console.WriteLine(i);
+            double f = random.NextDouble();
+            System.Console.WriteLine(f);
+            double result = Math.Ceiling(f)/100;
+            System.Console.WriteLine(result);
+            result = Math.Floor(f)/100;
+            System.Console.WriteLine(result);
+            result = Math.Round(f)/100;
+            System.Console.WriteLine(result);
+        }
+    }
+}
