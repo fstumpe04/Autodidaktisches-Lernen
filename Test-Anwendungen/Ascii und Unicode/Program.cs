f093055952704848451092c813a68a2ea2b9f180
Florian Stumpe <fstumpe04@gmail.com>
2022-08-01 17:49:47 +0200

    Video 76 Ascii und Unicode hinzugefügt.

---
+++ Test-Anwendungen/Ascii und Unicode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Ascii_und_Unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\u0026", Encoding.Unicode);
+        }
+    }
+}
